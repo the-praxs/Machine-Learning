4c7edfa1d7a78a9acc2a2367dc3be592d9230073
Pratyush Shukla <ps4534@nyu.edu>
2021-09-14 04:09:06 -0400

    Completed Problem 1 part 1
    Created plots for degree of polynomial d and saved figures in Figures subfolder.

---
+++ HW1/problem1_solution.m
@@ -0,0 +1,21 @@
+clear all;
+
+% Set path for saving figures
+path = "N:\\My Drive\\Repositories\\ece-gy-6143\\HW1\\Figures\\";
+
+% Load the dataset, which contains a structure
+dataset = load('problem1.mat');
+
+% Assigning structure values to variables
+X = dataset.x;
+y = dataset.y;
+
+% Checking for the dimension d from 1 to 7 to determine overfitting
+for D=1:7
+    [err, model] = polyreg(X, y, D);       % Returns the error and model coefficients
+    title(sprintf('Degree of polynomial (d) = %d', D));     % Saving title for plot with string formatting
+    filename = sprintf(path + 'fig_d_%d.jpeg', D);      % Figure files are saved in Figures subfolder
+    print(filename, '-djpeg');      % Saving the figures as .jpeg format for high-quality, low space requirement.
+end
+
+close all;
