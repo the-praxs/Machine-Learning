7f420b2ee32ae62ffaa533979d7307459cf6e485
Pratyush Shukla <ps4534@nyu.edu>
2021-09-14 04:16:50 -0400

    Modified problem1_solution.m
    Added code for saving workspace variables.

--- HW1/problem1_solution.m
+++ HW1/problem1_solution.m
@@ -1,4 +1,4 @@
-clear all;
+clear variables;
 
 % Set path for saving figures
 path = "N:\\My Drive\\Repositories\\ece-gy-6143\\HW1\\Figures\\";
@@ -13,9 +13,14 @@ y = dataset.y;
 % Checking for the dimension d from 1 to 7 to determine overfitting
 for D=1:7
     [err, model] = polyreg(X, y, D);       % Returns the error and model coefficients
+    
     title(sprintf('Degree of polynomial (d) = %d', D));     % Saving title for plot with string formatting
     filename = sprintf(path + 'fig_d_%d.jpeg', D);      % Figure files are saved in Figures subfolder
-    print(filename, '-djpeg');      % Saving the figures as .jpeg format for high-quality, low space requirement.
+    print(filename, '-djpeg');      % Saving the figures as .jpeg format for high-quality, low space requirements
+    
+    save('problem1_part1_values.mat', 'err', 'model');      % Save workspace variables
 end
 
 close all;
+
+
